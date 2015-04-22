67cd1986d12df9df82323b64595f8227cd55b385
christianhansson <mrc.hansson@gmail.com>
2015-04-22 12:08:17 +0200

    kommentarer gjorda på allt

--- src/TictactoeGame/GameModel.java
+++ src/TictactoeGame/GameModel.java
@@ -5,30 +5,74 @@ import java.util.Observable;
 
 public class GameModel extends Observable {
 
-	
+	/* Array that holds the positions and values of the game board */
 	public String [] btnValues = new String[9];
+	/* Combinations of win */
 	public int [][] winCombos = new int[][] {
 			{0,1,2},{3,4,5},{6,7,8},//Horizontal criteria of win.
 			{0,3,6},{1,4,7},{2,5,8},//Vertical criteria of win.
 			{2,4,6},{0,4,8}//Diagonal criteria of win.
 			};
-	
+	/* value that will become true if a player has won. default is false */
 	public boolean win = false;
+	/* Value that will become true if noone has won. */
 	public boolean noOneHasWon = false;
+	/* string that will hold the name of playerone in the current game- */
 	public String playerOne;
+	/* sting that will hold the name of playertwo in the current game. */
 	public String playerTwo;
-	public boolean isPlayersSet = false;
+	/* value that stands for if both usernames is set. */
+	public boolean isPlayerNamessSet = false;
+	/* Value that represents who's turn it is. */
 	public boolean turn = true;
-	
-	public boolean checkIfNoOneHasWon(){
-		return noOneHasWon;
+	/* if this value becomes true a new game will be created. */
+	public boolean newGame = false;
+	/**Set player one's name.
+	 * @param: String from controler with name of player one.
+	 * */
+	public void setPlayerOneName(String inName){
+		playerOne = inName;
+		setChanged();
+		notifyObservers();
 	}
+	/**Set player two's name.
+	 * @param: String from controler with name of player two.
+	 * */
+	public void setPlayerTwoName(String inName){
+		playerTwo = inName;
+		setChanged();
+		notifyObservers();
+	}
+	/**Method that will check if the players name is set. The it will notify observer.
+	 * */
+	public void playersIsSet(){
+		if(playerOne != null && playerTwo != null){		
+			isPlayerNamessSet = true;
+			setChanged();
+			notifyObservers();
+		}
+	}
+	/**Returns true or false depending on if players names is set.
+	 * */
+	public boolean checkPlayerNames(){
+		return isPlayerNamessSet;
+	}
+	/**Method will run if no one has won. notifying the observers.
+	 * */
 	public void setIfNoOneHasWon(){
 		noOneHasWon = true;
 		setChanged();
 		notifyObservers();
 	}
-	
+	/**Returning the value noOneHasWon. true or false.
+	 * */
+	public boolean checkIfNoOneHasWon(){
+		return noOneHasWon;
+	}
+	/**Method will run if a player has won.
+	 * @param: Symbol of the player who has won. And evaluating it and sending the winning
+	 * players name to the View via notifyobserver call.
+	 * */
 	public void winBecomesTrue(String inPlayer){
 		win = true;
 		setChanged();
@@ -38,73 +82,54 @@ public class GameModel extends Observable {
 			notifyObservers(playerTwo);
 		}
 	}
+	/**Returns true or false depending on if a player has won or not.
+	 * */
 	public boolean checkForWin(){
 		return win;
 	}
-	
+	/**Set specific value in the array btnValues to the incoming character, "X" or "O".
+	 * notifying the observer.
+	 *@param: the position where the symbol will be inserted to. is retrieved by the name of the
+	 *current button being pressed.
+	 *@param: Incoming symbol depending on who's turn it is.
+	 * */
 	public void setBtnValue(int pos, String symbol){
 		btnValues[pos] = symbol;
 		setChanged();
 		notifyObservers();
 	}
-	/*
-	 * Represents players names.
-	 * */
-	public void playersIsSet(){
-		if(playerOne != null && playerTwo != null){		
-			isPlayersSet = true;
-			setChanged();
-			notifyObservers();
-		}
-	}
-	public boolean checkPlayerNames(){
-		return isPlayersSet;
-	}
-	/*
-	 * Variable that is true if its playerOne's turn
-	 * */
-	/*
-	 * Will return variable turn.
+	/**Returns true or false. representing who's turn it is.
 	 * */
 	public boolean checkTurn(){
 		return turn;
 	}
-	/*
-	 * Runs if value "turn" is true, meaning that its player one's turn.
-	 * Sets turn to false.
+	/**sets the value of turn to false. Indicating that its the other players turn
 	 * */
 	public void turnIsTrue(){
 		turn = false;
 		setChanged();
 		notifyObservers();
 	}
-	/*
-	 * Runs if value "turn" is false, meaning that its player two's turn.
-	 * Sets turn to true.
+	/**sets the value of turn to true. Indicating that its the other players turn
 	 * */
 	public void turnIsFalse(){
 		turn = true;
 		setChanged();
 		notifyObservers();
 	}
-	public void setPlayerOneName(String inName){
-		playerOne = inName;
-		setChanged();
-		notifyObservers();
-	}
-	public void setPlayerTwoName(String inName){
-		playerTwo = inName;
-		setChanged();
-		notifyObservers();
-	}
-	public boolean newGame = false;
+	/**Sets the value of newGame to false.
+	 * */
 	public void newGameOngoing(){
 		newGame = false;
 	}
+	/**Returns the value of newGame.
+	 * */
 	public boolean doNewGame(){
 		return newGame;
 	}
-	/* Set the game state to new game, resets all in model */
+	/**This method represents the game state of when a new game is created.
+	 * Resets all the variables and clears the array of values.
+	 * */
 	public void setNullGameState(){
 		newGame = true;
 		playerOne = null;
